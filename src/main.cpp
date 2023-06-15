2aa9df137b9c6145794ae5f543007f7a962278b9
garrettgeesink <chatmansave@gmail.com>
2023-06-15 16:16:09 -0700

    finished driver

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,57 @@ using namespace Load;
 using namespace FeatureSelection;
 
 int main() {
-	string filename;
-	cout << "Welcome to the Geesink Twins' Feature Selection algorithm\n";
-	cout << "Type in the name of the file to test: ";
-	cin >> filename;
-	cout << "\n";
+	string choice = "0";
+ 	vector<vector<double>> data;
 
-	string algorithm;
-	cout << "Type the number of the algorithm you want to run\n";
+	cout << "Welcome to the Geesink Twins' Feature Selection algorithm\n";
+
+	while(data.size() == 0){
+		string filename;
+		cout << "Type in the name of the file to test: ";
+		cin >> filename;
+		vector<vector<double>> data = loadFile(filename);
+	}
+	
+
+
+	cout << "\nType the number of the algorithm you want to run\n";
 	cout << "1. Forward Selection\n";
 	cout << "2. Backward Selection\nChoice:";
-	cin >> algorithm;
+	Algorithm algorithm;
 
-	vector<vector<double>> data = loadFile(filename);
-	set<int> relevantFeatures = featureSearch(data);
-	double accuracy = kFoldCrossValidation(data.size(), data, currentSet, featureToAdd);
-	cout << accuracy << "\n";
-	//accuracy = floorf(accuracy*1000)/1000;
+	while(choice == "0") {
+		
+		cin >> choice;
+		
+		if(choice == 1)
+			algorithm == FORWARD;
+		else if(choice == 2)
+			algorithm = BACKWARD;
+		else {
+			cout << "Please input a valid choice";
+			choice = "0";
+		}
+	}
+	
+		
+
+	int numFolds;
+
+	cout << "\nHow many folds would you like for k-fold cross validation?\n";
+	cout << "1. n folds";
+	cout << "2. n/2 folds";
+	cout << "3. Custom amount\n";
+	cin >> choice;
+
+	if (choice == "1")
+		numFolds = data.size();
+	else if (choice == "2")
+		numFolds = data.size()/2;
+	else if (choice == "3") {
+		cout << "Please provide the integer number of folds: "
+		cin >> numFolds;
+	}
+	set<int> relevantFeatures = featureSearch(data, numFolds, algorithm);
 	return 0;
 }
