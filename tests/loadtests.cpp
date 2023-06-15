c770892bad3bfe9a9e8dff4e06e8998533765709
roveredpwn <roverdog56@gmail.com>
2023-06-14 17:34:34 -0700

    Created files for dataset loading and NN algorithm

---
+++ tests/loadtests.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(LoadTests, DummyTest) {
+	EXPECT_EQ(0, 0);
+}
---
+++ tests/nearestneighbortests.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(NearestNeighborTests, DummyTest) {
+	EXPECT_EQ(0, 0);
+}
