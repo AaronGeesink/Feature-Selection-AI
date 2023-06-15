f5dc3b9b6393ed275ba2ef1b555d05c01e641e8c
garrettgeesink <chatmansave@gmail.com>
2023-06-14 20:08:05 -0700

    function to load from file

--- header/load.h
+++ header/load.h
@@ -3,13 +3,15 @@
 
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <cmath>
 #include <string>
 
 using namespace std;
 
 namespace load {
-    double ieeeStringToDouble(string str) {
+    //Converts ieee string to double (not needed)
+    double ieeeStringToDouble(std::string str) {
         // Extracting the significand and exponent parts from the string
         size_t ePos = str.find('e');
         std::string significandStr = str.substr(0, ePos);
@@ -25,6 +27,39 @@ namespace load {
         // Calculating the final value
         return significand * pow(10, exponent);
     }
+
+    // Load a all values in file and return them as a 2D vector
+    std::vector<std::vector<double>> loadFile(string file) {
+        std::vector<std::vector<double>> data;
+        std::ifstream inputFile(file);
+        if (!inputFile) {
+            std::cerr << "Failed to open the file." << std::endl;
+            return data;
+        }
+
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            std::istringstream iss(line);
+            std::vector<double> row;
+
+            double value;
+            while (iss >> value) {
+                row.push_back(value);
+            }
+            data.push_back(row);
+        }
+
+        // Print the loaded data
+        // for (const auto& row : data) {
+        //     for (const auto& value : row) {
+        //         std::cout << value << " ";
+        //     }
+        //     std::cout << std::endl;
+        // }
+
+        return data;
+    }
+
 }
 
 #endif
